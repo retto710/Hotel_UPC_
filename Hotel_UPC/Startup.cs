8e5e4bb11dd9d20d78648d21859590acfc7dafb5
ipconfig <LAB_VH_207_PC11@PCVIVH207-11>
2018-05-23 07:49:36 -0500

    First

---
+++ Hotel_UPC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hotel_UPC.Startup))]
+namespace Hotel_UPC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
